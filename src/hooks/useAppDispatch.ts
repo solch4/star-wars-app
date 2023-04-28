c9ae5495546902e28a85319f38bf0238f663c8b0
solch4 <sol.maldonado.0209@gmail.com>
2023-04-28 03:36:37 -0300

    CharactersAction added to useAppDispatch hook

--- src/hooks/useAppDispatch.ts
+++ src/hooks/useAppDispatch.ts
@@ -3,9 +3,10 @@ import { ThunkDispatch } from "redux-thunk";
 import { Action } from "redux";
 import { State } from "../redux";
 import { FilmsAction } from "../redux/actions/films";
+import { CharactersAction } from "../redux/actions/characters";
 
 export const useAppDispatch = () => {
   const dispatch =
-    useDispatch<ThunkDispatch<State, null, Action<FilmsAction>>>();
+    useDispatch<ThunkDispatch<State, null, Action<FilmsAction | CharactersAction>>>();
   return dispatch;
 };
